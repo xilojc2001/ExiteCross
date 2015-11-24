d6f203556ae9926fbe2d936956c84a49bfe04195
Jorge Castro <JorgeCastro@jorges-macbook-pro.local>
2015-11-23 20:43:38 -0500

    Initial Commit

---
+++ ExiteCrossTests/ExiteCrossTests.swift
@@ -0,0 +1,36 @@
+//
+//  ExiteCrossTests.swift
+//  ExiteCrossTests
+//
+//  Created by Jorge Castro on 11/23/15.
+//  Copyright © 2015 Xilo. All rights reserved.
+//
+
+import XCTest
+@testable import ExiteCross
+
+class ExiteCrossTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
